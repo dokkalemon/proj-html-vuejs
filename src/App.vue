b99fc23123db0e107bbc9765f260a79221d8dd9b
dokkalemon <dottori.roberto@gmail..com>
2021-12-09 11:46:44 +0100

    add Main

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
     <Header />
+
+    <Main />
+
+
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
+//FontAwesome Package
 import '@fortawesome/fontawesome-free/css/all.css'
+
+//Components
+import Header from '@/components/Header.vue'
+import Main from '@/components/Main.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Main
     
   }
 }
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+      qui Main
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style scoped lang="scss">
+//import Vars
+@import '../styles/vars.scss';
+
+
+
+</style>
