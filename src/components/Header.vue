19c3d3d1f457a7d44a71bc42e1a0f8219df3e981
dokkalemon <dottori.roberto@gmail..com>
2021-12-09 11:28:41 +0100

    add logo and button on Header components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,16 @@
 <template>
-  <header>
-      qui header
+  <header class="full-width">
+      <div class="header container debug px-15">
+          <div class="header-logo">
+              <img src="../assets/logo_seo_w_1x.png" alt="">
+          </div>
+          <nav class="header-link debug">
+
+          </nav>
+          <div class="header-button">
+              <button class="button-primary">Get in touch now</button>
+          </div>
+      </div>
   </header>
 </template>
 
@@ -11,5 +21,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.header {
+    height: 100px;
+    display: flex;
+    .header-logo {
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+    .header-link {
+        flex-grow: 1;
+    }
+    .header-button {
+        display: flex;
+        align-items: center;
+    }
+}
 
 </style>
