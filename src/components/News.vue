f5349a628a988de536d2c66be4a9d016b54d9107
dokkalemon <dottori.roberto@gmail.com>
2021-12-10 10:48:00 +0100

    add Post Component into News component

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,17 +1,23 @@
 <template>
   <section class="news container px-15 debug">
     <Title title="Our News" />
+
+    <div class="posts">
+      <Post />
+    </div>
   </section>
 </template>
 
 <script>
 import Title from '@/components/Title.vue'
+import Post from '@/components/Post.vue'
 
 export default {
     name: 'News',
     
     components: {
-      Title
+      Title,
+      Post
     }
 }
 </script>
---
+++ src/components/Post.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="post">
+        
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Post',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
