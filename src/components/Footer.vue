9d0372961383d802e65fa78387bb76995992f374
dokkalemon <dottori.roberto@gmail.com>
2021-12-10 12:42:34 +0100

    add Social Icon into Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="full-width">
-      <div class="footer container px-15 debug">
+      <div class="footer container px-15">
           <div class="footer-logo">
               <img src="../assets/logo_seo_1x.png" alt="">
           </div>
@@ -15,6 +15,18 @@
                   <li><a href="">Home</a></li>
                   <li><a href="">Home</a></li>
               </ul>
+          </div>
+
+          <div class="copyright">
+              <p>© Copyright 2012 -2020 | Avada Theme by <a href="">ThemeFusion</a> | All Rights Reserved | Powered by <a href="">WordPress</a></p>
+          </div>
+
+          <div class="footer-socials">
+              <i class="fab fa-facebook-f"></i>
+              <i class="fab fa-instagram"></i>
+              <i class="fab fa-twitter"></i>
+              <i class="fab fa-youtube"></i>
+
           </div>
 
 
@@ -67,9 +79,43 @@ footer {
                         }
                     }
                     .active {color: $primary-color}
+                    &:last-child {
+                        margin-right: 0;
+                    }
                 }
             }
 
+        }
+        .copyright {
+            width: 100%;
+            text-align: center;
+            p {
+                font-size: 12px;
+                color: #8a8a8a;
+                a {
+                    color: $primary-title
+                }
+            }
+        }
+
+        .footer-socials {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            padding-top: 40px;
+            i {
+                color: $primary-text;
+                margin-right: 20px;
+                font-size: 22px;
+                cursor: pointer;
+                transition: all .4s ease;
+                &:hover {
+                    color: $primary-color;
+                }
+                &:last-child {
+                    margin-right: 0;
+                }
+            }
         }
     }
 }
