358181ddc5da6f3f66cb04ad849361e67c27ba88
dokkalemon <dottori.roberto@gmail.com>
2021-12-10 11:16:47 +0100

    add Testimonials components

---
+++ src/components/Testimonials.vue
@@ -0,0 +1,29 @@
+<template>
+<section class="testimonials full-width">
+    <div class="testimonials-container container px-15 debug">
+
+
+    </div>
+    
+</section>
+
+
+
+</template>
+
+<script>
+export default {
+    name: 'Testimonials'
+}
+</script>
+
+<style scoped lang="scss">
+//import var
+@import '../styles/vars.scss';
+
+
+
+
+
+
+</style>
