3f1212e21e3018f5f8a9e27a9e1825c56d48c486
dokkalemon <dottori.roberto@gmail.com>
2021-12-10 09:53:56 +0100

    add Title Components in Plans Component

--- src/components/Plans.vue
+++ src/components/Plans.vue
@@ -1,19 +1,56 @@
 <template>
-  <section class="plans">
+  <section class="plans full-width">
+      <div class="plans-cover">
+          <img src="../assets/background1.jpg" alt="">
+      </div>
 
-
+        <div class="plans-container container px-15">
+            <Title :secondarySet="true"
+            title="Our Plans"
+            />
+        </div>
+        
 
 
   </section>
 </template>
 
 <script>
+import Title from '@/components/Title.vue'
+
 export default {
-    name: 'Plans'
+    name: 'Plans',
+    components: {
+        Title
+    }
 }
 </script>
 
 <style scoped lang="scss">
+//import vars
+@import '../styles/vars.scss';
 
+.plans {
+    height: 800px;
+    position: relative;
+    padding-top: 100px;
+    .plans-cover {
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        img {
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+        }
+    }
+    .plans-container {
+        text-align: center;
+        position: relative;
+        z-index: 2;
+    }
+    
+}
 
 </style>
